3cbb4ce588d00145f4a25fe218d2943a98edc750
Kerrin Hardy <info@kerrinhardy.com>
2019-08-04 14:01:41 +0930

    Create Ignite Model Command

---
+++ src/Commands/IgniteModelCommand.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Kerrinhardy\Ignite\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+
+class IgniteModelCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ignite:model {name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create CRUD operations including Model, Migration, Controller and Request.';
+
+    /**
+     * Get the relevant stub file for the command.
+     */
+    protected function getStub($type)
+    {
+        return file_get_contents(resource_path("stubs/$type.stub"));
+    }
+
+    /**
+     * Create a new Model from the stub and the name entered in the command.
+     *
+     * @var string
+     */
+    protected function model($name)
+    {
+        $modelTemplate = str_replace(
+            ['{{modelName}}'],
+            [$name],
+            $this->getStub('Model')
+        );
+
+        file_put_contents(app_path("/{$name}.php"), $modelTemplate);
+    }
+
+    /**
+     * Create a new Controller from the stub and the name entered in the command.
+     *
+     * @var string
+     */
+    protected function controller($name)
+    {
+        $controllerTemplate = str_replace(
+            [
+                '{{modelName}}',
+                '{{modelNamePluralLowerCase}}',
+                '{{modelNameSingularLowerCase}}'
+            ],
+            [
+                $name,
+                strtolower(Str::plural($name)),
+                strtolower($name)
+            ],
+            $this->getStub('Controller')
+        );
+
+        file_put_contents(app_path("/Http/Controllers/{$name}Controller.php"), $controllerTemplate);
+    }
+
+    /**
+     * Create a new Request from the stub and the name entered in the command.
+     *
+     * @var string
+     */
+    protected function request($name)
+    {
+        $requestTemplate = str_replace(
+            ['{{modelName}}'],
+            [$name],
+            $this->getStub('Request')
+        );
+
+        if(!file_exists($path = app_path('/Http/Requests')))
+            mkdir($path, 0770, true);
+
+        file_put_contents(app_path("/Http/Requests/{$name}Request.php"), $requestTemplate);
+    }
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $name = $this->argument('name');
+
+        $this->controller($name);
+        $this->model($name);
+        $this->request($name);
+
+        File::append(base_path('routes/api.php'), 'Route::resource(\'' . strtolower(Str::plural($name)) . "', '{$name}Controller');");
+    }
+}
